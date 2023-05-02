a7afa0998642b28d33e890a3f1854ab729e68c46
Intern 37 <asandilelangeni@gmail.com>
2023-05-02 12:33:10 +0200

    snap

--- src/components/ContactComp.vue
+++ src/components/ContactComp.vue
@@ -1,92 +1,76 @@
-<template lang="">
-  <div class="Container0-fluid">
-  <h1>CONTACT</h1>
-  <div class="container1-fluid">
-      <form class="form" action="https://formspree.io/f/mgebedqd" method="POST">
-              <div class="mb-3 text-center">
-              <input type="text" class="form-control" id="exampleInputName1" name="Name" aria-describedby="nameHelp" placeholder="Enter your name" required v-model="name">
-              </div>
-              <div class="mb-3 text-align-center">
-              <input type="text" class="form-control" id="exampleInputSurname1" name="Surname" aria-describedby="emailHelp" placeholder="Enter your surname" required v-modal="surname">
-              <div id="emailHelp" class="form-text"></div>
-              </div>
-              <div class="mb-3 text-align-center">
-              <input type="email" class="form-control" id="exampleInputEmail1" name="Email" placeholder="Enter your email" required v-modal="Email">
-              <div id="messagedHelp" class="form-text"></div>
-              </div>
-              <div class="mb-4">
-              <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-              <label for="floatingTextarea">Comments</label>
-              </div>
-              <input type="submit" id="submit" placeholder="Submit" class="submit">
-
-         
+<template>
+  <div class="container-fluid">
+    <h1>CONTACT</h1>
+    <div class="container">
+      <form class="form" action="https://formspree.io/f/mgebedqd" method="POST" @submit.prevent="handleSubmit">
+        <div class="mb-3 text-center">
+          <input type="text" class="form-control" name="Name" placeholder="Enter your name" required v-model="name">
+        </div>
+        <div class="mb-3 text-align-center">
+          <input type="text" class="form-control" name="Surname" placeholder="Enter your surname" required v-model="surname">
+        </div>
+        <div class="mb-3 text-align-center">
+          <input type="email" class="form-control" name="Email" placeholder="Enter your email" required v-model="email">
+        </div>
+        <div class="mb-4">
+          <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+          <label for="floatingTextarea">Comments</label>
+        </div>
+        <input type="submit" value="Submit" class="submit">
       </form>
-  </div>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
   name: 'Contact',
   data() {
-      return {
-          name: '',
-          surname: '',
-          email: '',
-          message: '',
-      }
+    return {
+      name: '',
+      surname: '',
+      email: ''
+    }
   },
   methods: {
-      handleSubmit(e) {
-          e.preventDefault()
-          alert(`Name: ${this.name}, Surname: ${this.surname}, Email: ${this.email}, Message: ${this.message}`)
-      }
+    handleSubmit() {
+      alert(`Name: ${this.name}, Surname: ${this.surname}, Email: ${this.email}`)
+    }
   }
 }
 </script>
+
 <style scoped>
-/* input{
-  width: 100%;
-} */
-.container1{
-  width: 35rem;
+.container {
+  width: 45rem;
+  margin: auto;
 }
-form {
-  justify-content: center;
-  /* width: 35rem; */
-  margin:auto;
-  width: 100%;
-  
-}
-.form{
+.form {
   border-style: outset;
   padding: 4rem;
-  margin:0, 30rem;
-  border-radius: 5px;
-  width: 45rem;
+  width: 100%;
 }
 @media screen and (max-width: 580px) {
-  .form{
-      width: 28rem;
+  .form {
+    width: 28rem;
   }
 }
 @media screen and (max-width: 461px) {
-  .form{
-      width: 24rem;
+  .form {
+    width: 24rem;
   }
 }
 @media screen and (max-width: 480px) {
-.form{
-  width: 20rem}
-};
+  .form {
+    width: 20rem;
+  }
+}
 @media screen and (max-width: 350px) {
-  .form{
-      width: 18rem}
-  };
-.submit{
+  .form {
+    width: 18rem;
+  }
+}
+.submit {
   color: rgb(7, 47, 7);
 }
-.container0{
-  height: 90vh;
-}
-</style>
+</style>```
--- src/components/ResumeComp.vue
+++ src/components/ResumeComp.vue
@@ -1,200 +1,164 @@
-<template lang="">
-        <h1>RESUME</h1>
-    <div class="Resume">
-      <div class="timeline">
-        <div class="container left">
-    <div class="content">
-      <h2>2022</h2>
-      <p>Name of School: Life Choices Academy <br>
-        Duration: Current
-       
-      </p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2021</h2>
-      <p>Name of Company: ACDC Dynamics <br>
-        Duration: November 2021-September 2022 <br>
-        Reason for leaving: I left to study </p>
-    </div>
-  </div>
-  <div class="container left">
-    <div class="content">
-      <h2>2019</h2>
-      <p>Name of Company: IkamvaYouth <br>
-        Duration: July 2019-December 2019 <br>
-        Reason for leaving: Contract Ended
-      </p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2018</h2>
-      <p>Name of Company: Levi's Strauss <br>
-        Duration: May 2018-October 2018<br>
-        Reason for leaving: Contract Ended </p>
-    </div>
-  </div>
-  <div class="container left">
-    <div class="content">
-      <h2>2015-2020</h2>
-      <p>Name of School: University of the Western Cape <br>
-        Qualification: Bachelor of Commerce <br>
-        Majors: Economics and Information Systems
-      </p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2012-2014</h2>
+<template>
+  <!--This is the main container that contains the whole timeline.-->
+  <section class="design-section">
+    <div class="timeline">
+      <div class="timeline-empty"></div>
+      <div class="timeline-middle">
+        <div class="timeline-circle"></div>
+      </div>
+      <div class="timeline-component timeline-content">
+        <h2>2022</h2>
+        <p>
+          Name of School: Life Choices Academy <br />
+          Duration: Current
+        </p>
+      </div>
+      <div class="timeline-component timeline-content">
+        <h2>2021</h2>
+        <p>
+          Name of Company: ACDC Dynamics <br />
+          Duration: November 2021 - September 2022 <br />
+          Reason for leaving: I left to study
+        </p>
+      </div>
+      <div class="timeline-middle">
+        <div class="timeline-circle"></div>
+      </div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-middle">
+        <div class="timeline-circle"></div>
+      </div>
+      <div class="timeline-component timeline-content">
+        <h2>2019</h2>
+        <p>
+          Name of Company: IkamvaYouth <br />
+          Duration: July 2019 - December 2019 <br />
+          Reason for leaving: Contract Ended
+        </p>
+      </div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-middle">
+        <div class="timeline-circle"></div>
+      </div>
+      <div class="timeline-component timeline-content">
+        <h2>2018</h2>
+        <p>
+          Name of Company: Levi's Strauss <br />
+          Duration: May 2018 - October 2018<br />
+          Reason for leaving: Contract Ended
+        </p>
+      </div>
+      <!-- <div class="timeline-empty"></div> -->
+      <div class="timeline-middle">
+        <div class="timeline-circle"></div>
+      </div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-middle">
+        <div class="timeline-circle"></div>
+      </div>
+      <div class="timeline-component timeline-content">
+        <h2>2015-2020</h2>
+        <p>
+          Name of School: University of the Western Cape <br />
+          Qualification: Bachelor of Commerce <br />
+          Majors: Economics and Information Systems
+        </p>
+      </div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-empty"></div>
+      <div class="timeline-middle">
+        <div class="timeline-circle"></div>
+      </div>
+      <div class="timeline-component timeline-content">
+        <h2>2012-2014</h2>
       <p>Name of School: J.S Skenjana S.S.S <br>
         Qualification: Matric <br>
         Subjects: Economics, Accounting, Business Studies, Maths</p>
+      </div>
     </div>
-  </div>
-    </div>
-    </div>
-
+  </section>
 </template>
 <script>
 export default {
-    name: 'Resume',
+  
 }
 </script>
 <style>
-.container {
-  margin-bottom: 50px;
-  width: 48%;
-  font-size: 20px;
-  font-weight: 400;
-}
-.content p {
-  font-size: 20px;
-  font-weight: 300;
-  color: #0c0c0c;
-}
- /* Apply styles to the Resume title */
- h1 {
-        font-size: 36px;
-        color: #333;
-      }
-      
-      /* Apply styles to the timeline container */
-      .timeline {
-        position: relative;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        margin: 0 auto;
-      }
 
-      /* The actual timeline (the vertical ruler) */
-.timeline::after {
-  content: '';
-  position: absolute;
-  width: 6px;
-  background-color: white;
-  top: 0;
-  bottom: 0;
-  left: 50%;
-  margin-left: -3px;
-  
-}
-      
-      /* Apply styles to the left container */
-      .container.left {
-        width: 50%;
-      }
-      
-      /* Apply styles to the right container */
-      .container.right {
-        width: 50%;
-      }
-      
-      /* Apply styles to the year headings */
-      .content h2 {
-        font-size: 30px;
-        color: #333;
-      }
-      
-      /* Apply styles to the company/school names */
-      .content p:first-child {
-        font-size: 18px;
-        color: #555;
-        font-weight: bold;
-        margin-bottom: 10px;
-      }
-      
-      /* Apply styles to the duration information */
-      .content p:not(:first-child) {
-        font-size: 25px;
-        color: #0c0c0c;
-        margin-bottom: 10px;
-      }
-      
-      /* Apply styles to the reason for leaving */
-      .content p:last-child {
-        font-style: italic;
-      }
-      .right{
-        left: 0%;
-        margin-top: 190px;
-      }
-
-      /* The circles on the timeline */
-.container::after {
-  content: '';
-  position: absolute;
-  width: 25px;
-  height: 25px;
-  right: -15px;
-  background-color: white;
-  border: 4px solid #FF9F55;
-  top: 60px;
-  border-radius: 50%;
-  z-index: 1;
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@200;300;400&display=swap');
+.design-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #0c0c0c;;
+  min-height: 100vh;
+  padding: 100px 0;
   
 }
 
-      /* Add arrows to the left container (pointing right) */
-.left::before {
-  content: " ";
-  height: 0;
-  position: absolute;
-  top: 22px;
-  width: 0;
-  z-index: 1;
-  right: 30px;
-  border: medium solid white;
-  border-width: 10px 0 10px 10px;
-  border-color: transparent transparent transparent white;
-}
-/* Add arrows to the right container (pointing left) */
-.right::before {
-  content: " ";
-  height: 0;
-  position: absolute;
-  top: 22px;
-  width: 0;
-  z-index: 1;
-  left: 30px;
-  border: medium solid white;
-  border-width: 10px 10px 10px 0;
-  border-color: transparent white transparent transparent;
-}
-/* Fix the circle for containers on the right side */
-.right::after {
-  left: -16px;
+.design {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-@media screen and (max-width: 600px) {
-  .timeline::after, .container::after, .left::before, .right::before {
-    display: none;
-  }
+.timeline {
+  width: 80%;
+  height: auto;
+  max-width: 800px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.timeline-content {
+  padding: 20px;
+  background: #1f1f1f;
+  -webkit-box-shadow: 5px 5px 10px #1a1a1a, -5px -5px 10px #242424;
+          box-shadow: 5px 5px 10px #1a1a1a, -5px -5px 10px #242424;
+  border-radius: 5px;
+  color: white;
+  padding: 1.75rem;
+  transition: 0.4s ease;
+  overflow-wrap: break-word !important;
+  margin: 1rem;
+  margin-bottom: 20px;
+  border-radius: 6px;
+}
+
+.timeline-component {
+  margin: 0px 20px 20px 20px;
+}
+
+@media screen and (min-width: 768px) {
   .timeline {
-    flex-direction: column;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: 1fr 3px 1fr;
+  }
+  .timeline-middle {
+    position: relative;
+    background-image: linear-gradient(45deg, #F27121, #E94057, #8A2387);
+    width: 3px;
+    height: 100%;
+  }
+  .main-middle {
+    opacity: 0;
+  }
+  .timeline-circle {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-image: linear-gradient(45deg, #F27121, #E94057, #8A2387);
+    -webkit-transform: translateX(-50%);
+            transform: translateX(-50%);
   }
 }
+  
 </style>
